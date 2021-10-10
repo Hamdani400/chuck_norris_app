395db5d6944b4094f80b8fc373def674943e9c61
Hamdani Dinul Haq <dinulhaqhamdani41@gmail.com>
2021-10-10 18:09:00 +0700

    set autocomplete

--- chuck_norris_jokes_generator/src/Parts/Body/Body.js
+++ chuck_norris_jokes_generator/src/Parts/Body/Body.js
@@ -11,6 +11,10 @@ const axios = require ('axios');
 
 export default function Body () {
   const [randomJoke, setRandomJoke] = useState ([]);
+  const [searchValue, setSearchValue] = useState ('');
+  const [categoryValue, setCategoryValue] = useState ('');
+  const [categoryList, setCategoryList] = useState ([]);
+  const [isSearchingCategory, setIsSearchingCategory] = useState (false);
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect (() => {
@@ -22,6 +26,33 @@ export default function Body () {
     });
   }, []);
 
+  useEffect (
+    () => {
+      if (categoryValue) {
+        setIsSearchingCategory (true);
+      } else {
+        setIsSearchingCategory (false);
+      }
+    },
+    [categoryValue]
+  );
+
+  useEffect (() => {
+    axios.get ('https://api.chucknorris.io/jokes/categories').then (res => {
+      setCategoryList (res.data);
+    });
+  }, []);
+
+  const getSearchValue = e => {
+    setSearchValue (e.target.value);
+    console.log ('aku anak' + searchValue);
+  };
+
+  const getCategoryValue = e => {
+    setCategoryValue (e.target.value);
+    console.log (categoryValue);
+  };
+
   const getRandomJoke = () => {
     axios.get ('https://api.chucknorris.io/jokes/random').then (res => {
       const datas = [];
@@ -31,17 +62,53 @@ export default function Body () {
     });
   };
 
+  const showCategory = categoryList
+    .filter (text => text.includes (categoryValue))
+    .map ((text, index) => {
+      return (
+        <ul
+          onClick={() => {
+            setCategoryValue (text);
+          }}
+          key={`category-${index}`}
+        >
+          {text}
+        </ul>
+      );
+    });
+
   return (
     <section>
       <div className="input-wrapper">
-        <InputText className={['search-input']} />
+        <InputText
+          className={['search-input']}
+          placeholder="Search jokes by text"
+          searchVal={getSearchValue}
+        />
         <Button text="Search!" width="91px" />
       </div>
       <div className="icon-wrapper">
         <ChuckNorrisIcon className="cn-icon" />
       </div>
       <Result text={randomJoke} className={['random-result']} />
-      <Button onClick={getRandomJoke} text="Another!" width="103px" />
+      <div className="random-button">
+        <Button onClick={getRandomJoke} text="Another!" width="103px" />
+      </div>
+      <div className="list-of-category">
+        {isSearchingCategory && showCategory}
+      </div>
+      <div className="input-wrapper category">
+        <InputText
+          className={['search-input']}
+          placeholder="Search jokes by category"
+          categoryVal={getCategoryValue}
+        />
+        <Button
+          text="Search!"
+          onClick={() => console.log (categoryValue)}
+          width="91px"
+        />
+      </div>
     </section>
   );
 }
