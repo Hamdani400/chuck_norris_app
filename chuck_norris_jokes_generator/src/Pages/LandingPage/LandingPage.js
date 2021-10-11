f3c4907f6e3f56aa2edb7bc7fc96854cb016c4e8
Hamdani Dinul Haq <dinulhaqhamdani41@gmail.com>
2021-10-11 10:17:11 +0700

    change window tab title

--- chuck_norris_jokes_generator/src/Pages/LandingPage/LandingPage.js
+++ chuck_norris_jokes_generator/src/Pages/LandingPage/LandingPage.js
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import Header from '../../Parts/Header/Header.js';
 import Body from '../../Parts/Body/Body.js';
 
 export default function LandingPage () {
+  useEffect (() => {
+    document.title = 'Chuck Norris Jokes | Home';
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <div>
       <Header />
